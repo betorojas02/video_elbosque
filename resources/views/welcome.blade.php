bfd9a8a76eb5e319937b76e5b802d2305bbb0105
beto <jhmorenor@unbosque.edu.co>
2018-08-13 21:54:25 -0500

    cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    beto Rojas
+                    Beto Rojas
                 </div>
 
                 <div class="links">
